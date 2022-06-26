59891d613242b4e09cee20bb6f9f4937d080598f
zachloh <leongaun17@gmail.com>
2022-06-26 18:15:15 +0800

    Add Home page component

--- src/routes/Home/Home.js
+++ src/routes/Home/Home.js
@@ -1,7 +1,26 @@
 import React from 'react';
+import heroImg from '../../assets/images/denim-models.jpg';
+import styles from './Home.module.css';
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.hero}>
+        <img
+          src={heroImg}
+          alt="a male and a female model wearing denims"
+          class={styles['hero-img']}
+        />
+        <div className={styles['hero-text-container']}>
+          <p className={styles['hero-text']}>DISCOVER FASHION</p>
+          <p className={styles['hero-text']}>
+            Better when it's on <i>you</i>
+          </p>
+          <button className={styles['hero-button']}>Go to shop</button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
