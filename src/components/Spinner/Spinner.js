da315f82c3e00ddd9c92d9d02be27a34232adc0e
zachloh <leongaun17@gmail.com>
2022-06-28 13:10:33 +0800

    Redesign spinner using svg

--- src/components/Spinner/Spinner.js
+++ src/components/Spinner/Spinner.js
@@ -4,7 +4,23 @@ import styles from './Spinner.module.css';
 const Spinner = () => {
   return (
     <div className={styles.container}>
-      <div className={styles.spinner} />
+      <svg
+        viewBox="0 0 24 24"
+        xmlns="<http://www.w3.org/2000/svg>"
+        height="100px"
+        width="100px"
+      >
+        <circle
+          cx="12"
+          cy="12"
+          r="8"
+          strokeLinecap="round"
+          strokeWidth="4"
+          stroke="#aaa"
+          fill="none"
+          className={styles.main}
+        />
+      </svg>
     </div>
   );
 };
